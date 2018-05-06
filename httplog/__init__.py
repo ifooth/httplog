c1488d56190282a3f60675a634e829e074481b54
JoeLei <joelei@tencent.com>
2018-05-07 01:57:55 +0800

    add settings

--- httplog/__init__.py
+++ httplog/__init__.py
@@ -1,44 +1,9 @@
-# -*- coding: utf-8 -*
-# Copyright 2015 Tencent
-# Author: Joe Lei <joelei@tencent.com>
-import logging.config
-import os.path
-
+# -*- coding: utf-8 -*-
+# Copyright 2018 IFOOTH
+# Author: Joe Lei <thezero12@hotmail.com>
 from httplog import patcher
 
 monkey_patch = patcher.monkey_patch
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'simple': {
-            'format': '[%(asctime)s] %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'httplog': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.expanduser('~') + '/httplog.log',
-            'formatter': 'simple'
-        }
-    },
-    'loggers': {
-        'httplog': {
-            'handlers': ['httplog'],
-            'level': 'DEBUG',
-            'propagate': True,
-        }
-    }
-}
-
-logging.config.dictConfig(LOGGING)
 
 
 __VERSION__ = '1.20'
