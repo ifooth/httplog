c96616623a76f90130a230b29bfc721a55d7b4ec
JoeLei <joelei@tencent.com>
2018-05-07 01:59:17 +0800

    settings

--- httplog/__init__.py
+++ httplog/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 IFOOTH
 # Author: Joe Lei <thezero12@hotmail.com>
+from httplog import settings  # noqa
 from httplog import patcher
 
 monkey_patch = patcher.monkey_patch
