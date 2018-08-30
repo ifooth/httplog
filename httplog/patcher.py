8e882bd3cda946df6a6c97bf146da7de53431b8d
JoeLei <joelei@tencent.com>
2018-08-30 16:57:56 +0800

    update httplog

--- httplog/patcher.py
+++ httplog/patcher.py
@@ -2,8 +2,11 @@
 # Copyright 2018 IFOOTH
 # Author: Joe Lei <thezero12@hotmail.com>
 """monkey patch"""
+import logging.config
 import traceback
+import copy
 
+from httplog import settings
 from httplog.support.httplib2 import Http
 from httplog.support.requests import Session
 from httplog.support.urllib2 import http_log_wraper
@@ -38,7 +41,19 @@ def patch_urlopen():
         traceback.print_exc()
 
 
-def monkey_patch(httplib2=True, requests=True, urlopen=True):
+def monkey_patch(httplib2=True, requests=True, urlopen=True, logfile='', logformat=''):
+    # 设置logging
+    log_conf = copy.deepcopy(settings.LOGGING)
+    if logfile:
+        # 使用文件
+        log_conf['loggers']['httplog']['handlers'] = ['file']
+        settings.FILE['filename'] = logfile
+        log_conf['handlers']['file'] = settings.FILE
+
+    if logformat:
+        log_conf['formatters']['simple']['format'] = logformat
+    logging.config.dictConfig(log_conf)
+
     if httplib2:
         patch_httplib2()
     if requests:
--- httplog/settings.py
+++ httplog/settings.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 IFOOTH
 # Author: Joe Lei <thezero12@hotmail.com>
-import logging.config
-import os.path
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -18,20 +15,19 @@ LOGGING = {
             'class': 'logging.StreamHandler',
             'formatter': 'simple'
         },
-        'httplog': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.expanduser('~') + '/httplog.log',
-            'formatter': 'simple'
-        }
     },
     'loggers': {
         'httplog': {
-            'handlers': ['httplog'],
+            'handlers': ['console'],
             'level': 'DEBUG',
             'propagate': True,
         }
     }
 }
 
-logging.config.dictConfig(LOGGING)
+FILE = {
+    'level': 'DEBUG',
+    'class': 'logging.FileHandler',
+    'filename': 'httplog.log',
+    'formatter': 'simple'
+}
